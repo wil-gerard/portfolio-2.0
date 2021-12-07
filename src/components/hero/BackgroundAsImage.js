7b7205747a7e729ec220b0e8fa3be762cdb51b39
Wil Gerard <williamrgerard@gmail.com>
2021-12-06 22:11:20 -0600

    edit: removed navbar and contact me button in hero for now

--- src/components/hero/BackgroundAsImage.js
+++ src/components/hero/BackgroundAsImage.js
@@ -3,23 +3,24 @@ import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
 import HeroImage from "images/hero-background.jpg";
-import { Subheading as Subheading } from "components/misc/Headings.js";
+import { Subheading } from "components/misc/Headings.js";
 import { ReactComponent as TwitterIcon } from "../../images/twitter-icon.svg";
 import { ReactComponent as LinkedinIcon } from "../../images/linkedin-icon.svg";
 import { ReactComponent as GithubIcon } from "../../images/github-icon.svg";
 import { css } from "styled-components/macro"; //eslint-disable-line
 
-import Header, { NavLink, NavLinks, PrimaryLink, LogoLink, NavToggle, DesktopNavLinks } from "../headers/light.js";
+// import Header, { NavLink, NavLinks, LogoLink, NavToggle, DesktopNavLinks } from "../headers/light.js";
 
-const StyledHeader = styled(Header)`
-  ${tw`pt-8 max-w-none`}
-  ${DesktopNavLinks} ${NavLink}, ${LogoLink} {
-    ${tw`text-gray-100 hover:border-gray-300 hover:text-gray-300`}
-  }
-  ${NavToggle}.closed {
-    ${tw`text-gray-100 hover:text-primary-500`}
-  }
-`;
+// const StyledHeader = styled(Header)`
+//   ${tw`pt-8 max-w-none`}
+//   ${DesktopNavLinks} ${NavLink}, ${LogoLink} {
+//     ${tw`text-gray-100 hover:border-gray-300 hover:text-gray-300`}
+//   }
+//   ${NavToggle}.closed {
+//     ${tw`text-gray-100 hover:text-primary-500`}
+//   }
+// `;
+
 const Container = styled.div`
   ${tw`relative -mx-8 -mt-8 bg-center bg-cover`}
   background-image: url(${HeroImage});
@@ -28,9 +29,9 @@ const Container = styled.div`
 const OpacityOverlay = tw.div`z-10 absolute inset-0 bg-primary-500 opacity-25`;
 
 const HeroContainer = tw.div`z-20 relative px-4 sm:px-8 max-w-screen-xl h-screen mx-auto`;
-const TwoColumn = tw.div`pt-24 pb-32 px-4 flex justify-between items-center flex-col lg:flex-row`;
+const TwoColumn = tw.div`pt-48 pb-32 px-4 flex justify-between items-center flex-col lg:flex-row`;
 const LeftColumn = tw.div`flex flex-col items-center lg:block`;
-const RightColumn = tw.div`w-full sm:w-5/6 lg:w-1/2 mt-16 lg:mt-0 lg:pl-8`;
+// const RightColumn = tw.div`w-full sm:w-5/6 lg:w-1/2 mt-16 lg:mt-0 lg:pl-8`;
 
 const SocialLinksContainer = tw.div`mt-6 `;
 const SocialLink = styled.a`
@@ -49,28 +50,28 @@ const Heading = styled.h1`
 
 const Notification = tw.span`inline-block my-4 pl-3 py-1 text-gray-100 border-l-4 border-blue-500 font-medium text-sm`;
 
-const PrimaryAction = tw.button`px-8 py-3 mt-5 text-sm sm:text-base sm:mt-8 sm:px-8 sm:py-4 bg-gray-100 text-primary-500 font-bold rounded shadow transition duration-300 hocus:bg-primary-500 hocus:text-gray-100 focus:shadow-outline`;
+// const PrimaryAction = tw.button`px-8 py-3 mt-5 text-sm sm:text-base sm:mt-8 sm:px-8 sm:py-4 bg-gray-100 text-primary-500 font-bold rounded shadow transition duration-300 hocus:bg-primary-500 hocus:text-gray-100 focus:shadow-outline`;
 
 export default () => {
-  const navLinks = [
-    <NavLinks key={1}>
-      <NavLink href="#">
-        About
-      </NavLink>
-      <NavLink href="#">
-        Portfolio
-      </NavLink>
-      <NavLink href="SimpleContactUs">
-        Contact
-      </NavLink>
-    </NavLinks>,
-  ];
+  // const navLinks = [
+  //   <NavLinks key={1}>
+  //     <NavLink href="#">
+  //       About
+  //     </NavLink>
+  //     <NavLink href="#">
+  //       Portfolio
+  //     </NavLink>
+  //     <NavLink href="#">
+  //       Contact
+  //     </NavLink>
+  //   </NavLinks>,
+  // ];
 
   return (
     <Container>
       <OpacityOverlay />
       <HeroContainer>
-        <StyledHeader links={navLinks} />
+        {/* <StyledHeader links={navLinks} /> */}
         <TwoColumn>
           <LeftColumn>
             <Notification>Based in Minneapolis, MN</Notification>
@@ -91,7 +92,7 @@ export default () => {
                 <TwitterIcon />
               </SocialLink>
             </SocialLinksContainer>
-            <PrimaryAction>Contact me</PrimaryAction>
+            {/* <PrimaryAction>Contact me</PrimaryAction> */}
           </LeftColumn>
         </TwoColumn>
       </HeroContainer>
