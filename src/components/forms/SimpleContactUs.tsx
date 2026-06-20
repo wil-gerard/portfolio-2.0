c41f5cf77d6f0e73e3a99057bc6bb846c4a4e779
Wil Gerard <williamrgerard@gmail.com>
2026-06-20 15:10:11 -0500

    fix(contact): resolve type errors in SimpleContactUs

--- src/components/forms/SimpleContactUs.tsx
+++ src/components/forms/SimpleContactUs.tsx
@@ -10,12 +10,6 @@ const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
 
 const FormContainer = styled.div`
   ${tw`p-10 sm:p-12 md:p-16 bg-primary-500 text-gray-100 rounded-lg relative`}
-  form {
-    ${tw`mt-4`}
-  }
-  h2 {
-    ${tw`text-3xl sm:text-4xl font-bold`}
-  }
   input,textarea {
     ${tw`w-full bg-transparent text-gray-100 text-base font-medium tracking-wide border-b-2 py-2 text-gray-100 hocus:border-pink-400 focus:outline-none transition duration-200`};
 
@@ -24,6 +18,8 @@ const FormContainer = styled.div`
     }
   }
 `;
+const Heading = tw.h2`text-3xl sm:text-4xl font-bold`;
+const Form = tw.form`mt-4`;
 
 const SocialLinksContainer = tw.div`mt-6 `;
 const SocialLink = styled.a.attrs({
@@ -45,14 +41,15 @@ const TextArea = tw.textarea`h-24 sm:h-full resize-none`;
 const SubmitButton = tw.button`w-full sm:w-32 mt-6 py-3 bg-gray-100 text-primary-500 rounded-full font-bold tracking-wide shadow-lg uppercase text-sm transition duration-300 transform focus:outline-none focus:shadow-outline hover:bg-gray-300 hover:text-primary-700 hocus:-translate-y-px hocus:shadow-xl`;
 
 const SvgDotPattern1 = tw(SvgDotPatternIcon)`absolute bottom-0 right-0 transform translate-y-1/2 translate-x-1/2 -z-10 opacity-50 text-primary-500 fill-current w-24`
+const InnerWrapper = tw.div`mx-auto max-w-4xl`
 
-export default () => {
+const SimpleContactUs = () => {
   return (
     <Container>
       <Content>
         <FormContainer>
-          <div tw="mx-auto max-w-4xl">
-            <h2>Contact Me</h2>
+          <InnerWrapper>
+            <Heading>Contact Me</Heading>
             <SocialLinksContainer>
               <SocialLink href="https://linkedin.com/in/wilgerard/">
                 <LinkedinIcon />
@@ -61,7 +58,7 @@ export default () => {
                 <GithubIcon />
               </SocialLink>
             </SocialLinksContainer>
-            <form method="POST" action="https://formspree.io/f/xgerkqlv">
+            <Form method="POST" action="https://formspree.io/f/xgerkqlv">
               <TwoColumn>
                 <Column>
                   <InputContainer>
@@ -85,7 +82,7 @@ export default () => {
                 </Column>
                 <Column>
                   <InputContainer tw="flex-1">
-                    <Label htmlFor="name-input">Your Message</Label>
+                    <Label htmlFor="message-input">Your Message</Label>
                     <TextArea
                       id="message-input"
                       name="message"
@@ -95,14 +92,16 @@ export default () => {
                 </Column>
               </TwoColumn>
 
-              <SubmitButton type="submit" value="Submit">
+              <SubmitButton type="submit">
                 Submit
               </SubmitButton>
-            </form>
-          </div>
+            </Form>
+          </InnerWrapper>
           <SvgDotPattern1 />
         </FormContainer>
       </Content>
     </Container>
   );
 };
+
+export default SimpleContactUs;
